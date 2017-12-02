b9eb2dfa538e92b3c615280b0cfc03c590480a25
prince rastogi <webpulse.prince@gmail.com>
2017-12-02 12:42:09 +0530

    create file toast

---
+++ toastlibrary/src/main/java/com/toastlibrary/ToastFile.java
@@ -0,0 +1,26 @@
+package com.toastlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by Server on 12/2/2017.
+ */
+
+public class ToastFile {
+    public void toast(final String message, final AppCompatActivity activity) {
+        try {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (message != null && !message.isEmpty())
+                        Toast.makeText(activity, message,
+                                Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
